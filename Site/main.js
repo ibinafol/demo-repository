788ec768c1494b8d2846976ee77d47a251cb8b1a
Murad <91017684+muuradad@users.noreply.github.com>
2024-03-15 13:11:42 +0300

    Delete Site directory

--- Site/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement,{
-         style: `
-         --move-x: ${(e.clientX - window.innerWidth/2) * -0.1}deg;
-         --move-y: ${(e.clientY - window.innerHeight/2) * -0.1}deg;
-
-         `
-    })
-})
